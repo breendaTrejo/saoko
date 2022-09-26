ae94e9c91c51da2be855ebfb52912280a720c635
breendaTrejo <breenda.trejo@gmail.com>
2022-09-26 11:48:41 -0500

    ci: creacion script

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+      console.log("saoko papi saoko");
+      document.getElementById("generation").style.color = "grey";
+      function colorTitulo(){
+        console.log("Pulso el boton generation");
+        document.getElementById("generation").style.color = "blue";
+      }
+  
+  
+      </script>
