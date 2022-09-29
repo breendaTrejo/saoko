f5b5dbf6a271b1a43f2d0bd62149dec923eb56ed
breendaTrejo <breenda.trejo@gmail.com>
2022-09-28 21:55:50 -0500

    ci: ejercicio: calculadora

---
+++ calculadorabien/script.js
@@ -0,0 +1,16 @@
+console.log(Práctica-Calculadora);
+
+let a = document.getElementById("num1").value;
+let b = document.getElementById("num2").value;
+
+/**Funcion suma */
+let suma = (a,b) => a+b;
+ 
+/**Funcion resta */
+let resta = (a,b) => a-b;
+
+/**Funcion multiplicación */
+let multiplicacion = (a,b) => a*b;
+
+/**Funcion división */
+let division = (a,b) => a/b;
